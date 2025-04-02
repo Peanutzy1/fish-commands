0f96562b687d0da18442a312c8bab426694d53dd
BalaM314 <71201189+BalaM314@users.noreply.github.com>
2025-04-02 17:43:05 +0530

    Remove april foolishness

--- src/index.ts
+++ src/index.ts
@@ -127,7 +127,7 @@ Events.on(EventType.ServerLoadEvent, (e) => {
 
 	// Mute muted players
 	Vars.netServer.admins.addChatFilter((player, message) => processChat(player, message));
-	Vars.netServer.admins.addChatFilter((p, message) => foolifyChat(message));
+
 	// Action filters
 	Vars.netServer.admins.addActionFilter((action:PlayerAction) => {
 		const player = action.player;
