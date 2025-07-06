3136d5eb5ebabb8bc31cbe4f45c61f4f2645ab62
BalaM314 <71201189+BalaM314@users.noreply.github.com>
2025-07-06 17:42:19 +0530

    oops

--- scripts/attach.js
+++ scripts/attach.js
@@ -1,35 +1,38 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-const fs = require("node:fs");
-const path = require("node:path");
+const node_fs_1 = __importDefault(require("node:fs"));
+const node_path_1 = __importDefault(require("node:path"));
 function fail(message) {
     console.error(message);
     process.exit(1);
 }
 if (!process.argv[2])
     fail(`Please provide the path to your server jar file. If you do not have one, run the "dev" script instead.`);
-const filepath = path.resolve(process.argv[2]);
+const filepath = node_path_1.default.resolve(process.argv[2]);
 try {
-    fs.accessSync(filepath, fs.constants.R_OK);
+    node_fs_1.default.accessSync(filepath, node_fs_1.default.constants.R_OK);
 }
 catch {
     fail(`Path "${filepath}" does not exist or is not accessible.`);
 }
-if (path.extname(filepath) != ".jar")
+if (node_path_1.default.extname(filepath) != ".jar")
     fail(`Path must point to a jar file.`);
-const configDir = path.join(filepath, "..", "config");
+const configDir = node_path_1.default.join(filepath, "..", "config");
 try {
-    if (!fs.statSync(configDir).isDirectory())
+    if (!node_fs_1.default.statSync(configDir).isDirectory())
         fail(`Config folder at "${configDir}" is not a directory. Are you sure this is a Mindustry server directory?`);
 }
 catch {
     fail(`Path "${configDir}" does not exist or is not accessible. Are you sure this is a Mindustry server directory?`);
 }
-const fishCommandsFolder = path.join(configDir, "mods", "fish-commands");
+const fishCommandsFolder = node_path_1.default.join(configDir, "mods", "fish-commands");
 try {
-    if (fs.lstatSync(fishCommandsFolder).isSymbolicLink()) {
+    if (node_fs_1.default.lstatSync(fishCommandsFolder).isSymbolicLink()) {
         //Symlink already exists, delete it
-        fs.unlinkSync(fishCommandsFolder);
+        node_fs_1.default.unlinkSync(fishCommandsFolder);
         console.log(`Unlinked fish-commands.`);
     }
     else {
@@ -38,8 +41,8 @@ try {
 }
 catch {
     //File does not exist, create a symlink
-    const buildPath = path.join(process.argv[1], "../../build");
+    const buildPath = node_path_1.default.join(process.argv[1], "../../build");
     console.log(`Creating symlink from "${fishCommandsFolder}" to "${buildPath}"`);
-    fs.symlinkSync(buildPath, fishCommandsFolder);
+    node_fs_1.default.symlinkSync(buildPath, fishCommandsFolder);
     console.log(`Successfully linked fish-commands.`);
 }
--- scripts/dev.js
+++ scripts/dev.js
@@ -1,8 +1,11 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-const fs = require("node:fs");
-const path = require("node:path");
-const https = require("node:https");
+const node_fs_1 = __importDefault(require("node:fs"));
+const node_path_1 = __importDefault(require("node:path"));
+const node_https_1 = __importDefault(require("node:https"));
 const node_child_process_1 = require("node:child_process");
 function fail(message) {
     console.error(message);
@@ -10,7 +13,7 @@ function fail(message) {
 }
 function resolveRedirect(url) {
     return new Promise((resolve, reject) => {
-        https.get(url, (res) => {
+        node_https_1.default.get(url, (res) => {
             if (res.statusCode != 302) {
                 if (res.statusCode == 404) {
                     reject("Version does not exist.");
@@ -30,14 +33,14 @@ function resolveRedirect(url) {
 }
 function downloadFile(url, outputPath) {
     return new Promise((resolve, reject) => {
-        https.get(url, (res) => {
+        node_https_1.default.get(url, (res) => {
             if (res.statusCode == 404) {
                 reject(`File does not exist.`);
             }
             else if (res.statusCode != 200) {
                 reject(`Expected status code 200, got ${res.statusCode}`);
             }
-            const file = fs.createWriteStream(outputPath);
+            const file = node_fs_1.default.createWriteStream(outputPath);
             res.pipe(file);
             file.on('finish', () => {
                 file.close();
@@ -46,11 +49,11 @@ function downloadFile(url, outputPath) {
         });
     });
 }
-const fcRootDirectory = path.join(process.argv[1], "..", "..");
-const devServerDirectory = path.join(fcRootDirectory, "dev-server");
-if (!fs.existsSync(devServerDirectory)) {
+const fcRootDirectory = node_path_1.default.join(process.argv[1], "..", "..");
+const devServerDirectory = node_path_1.default.join(fcRootDirectory, "dev-server");
+if (!node_fs_1.default.existsSync(devServerDirectory)) {
     console.log(`Dev server does not exist yet, creating one...`);
-    fs.mkdirSync(devServerDirectory, {
+    node_fs_1.default.mkdirSync(devServerDirectory, {
         recursive: false
     });
     console.log(`Finding latest server jar...`);
@@ -59,15 +62,15 @@ if (!fs.existsSync(devServerDirectory)) {
         console.log(`Downloading latest server jar from ${file.browser_download_url}...`);
         return resolveRedirect(file.browser_download_url);
     }).then(downloadURL => {
-        return downloadFile(downloadURL, path.join(devServerDirectory, "server-release.jar"));
+        return downloadFile(downloadURL, node_path_1.default.join(devServerDirectory, "server-release.jar"));
     }).catch(e => fail(`Failed to download the file: ${e}`)).then(() => {
         console.log(`Linking fish-commands...`);
-        const modsFolder = path.join(devServerDirectory, "config", "mods");
-        fs.mkdirSync(modsFolder, { recursive: true });
-        const fishCommandsFolder = path.join(modsFolder, "fish-commands");
-        const buildFolder = path.join(fcRootDirectory, "build");
-        fs.symlinkSync(buildFolder, fishCommandsFolder);
-        fs.writeFileSync(path.join(devServerDirectory, "config", ".debug"), "");
+        const modsFolder = node_path_1.default.join(devServerDirectory, "config", "mods");
+        node_fs_1.default.mkdirSync(modsFolder, { recursive: true });
+        const fishCommandsFolder = node_path_1.default.join(modsFolder, "fish-commands");
+        const buildFolder = node_path_1.default.join(fcRootDirectory, "build");
+        node_fs_1.default.symlinkSync(buildFolder, fishCommandsFolder);
+        node_fs_1.default.writeFileSync(node_path_1.default.join(devServerDirectory, "config", ".debug"), "");
         console.log(`Successfully set up the development environment.`);
         runServer();
     });
@@ -78,8 +81,9 @@ else {
 function runServer() {
     console.log("Starting fish-commands Mindustry development server...");
     const { status } = (0, node_child_process_1.spawnSync)(`which`, ["rlwrap"]);
-    (0, node_child_process_1.execSync)(`${status === 0 ? "rlwrap " : ""}java -Xmx500M -Xms500M -jar "server-release.jar"`, {
+    const memory = "1G";
+    (0, node_child_process_1.execSync)(`${status === 0 ? "rlwrap " : ""}java -Xmx${memory} -Xms${memory} -jar "server-release.jar"`, {
         stdio: "inherit",
-        cwd: path.join(fcRootDirectory, "dev-server")
+        cwd: node_path_1.default.join(fcRootDirectory, "dev-server")
     });
 }
