fedb91a349221be39d33af0d202432b4854f8804
BalaM314 <71201189+BalaM314@users.noreply.github.com>
2025-07-24 21:07:22 +0530

    make client file a .ts for ease of use

---
+++ src/client.ts
@@ -0,0 +1,5 @@
+/*
+Copyright © BalaM314, 2025. All Rights Reserved.
+This file is a client environment: you can write typescript code in this file and run a command to copy it to the clipboard, then run it in-game on your client.
+*/
+
